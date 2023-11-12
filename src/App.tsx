442908f0df434cfb804b9d8c50b11b8b0a7e6cf0
Sinchana P <sinchana.p.swarna@gmail.com>
2023-11-12 14:47:09 +0530

    Sider, Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,59 @@
 import { useState } from 'react'
-import './App.css'
-import { Button } from 'antd'
+import { Button, Layout, Menu } from 'antd'
+import { Content, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import "./styles/dashboard.css"
+import { HiOutlineHome } from 'react-icons/hi'
+import { GrOrganization } from 'react-icons/gr'
+import { BsPerson } from 'react-icons/bs'
+import { FaUsers } from 'react-icons/fa'
 
 function App() {
 
   return (
-    <div>
-      <Button type="primary">Test</Button>
-    </div>
+    <Layout className='container'>
+      <Header
+        style={{
+          backgroundColor: "white"
+        }}
+      >
+        <div className="brand">
+          Antd-Dashboard
+        </div>
+      </Header>
+      <Layout>
+        <Sider theme='light'>
+          <Menu 
+            mode='inline'
+            items = {[
+              {
+                label: "Home",
+                key: "home",
+                icon: <HiOutlineHome />,
+                children: [
+                  {
+                    label: "Add profile",
+                    key: "add_profile",
+                    icon: <BsPerson />
+                  },
+                  {
+                    label: 'All users',
+                    key: "all_users",
+                    icon: <FaUsers />
+                  },
+                ],
+              },
+              {
+                label: "About",
+                key: "about",
+                icon: <GrOrganization />
+              }
+            ]}
+          />
+        </Sider>
+        <Content>Content</Content>
+      </Layout>
+    </Layout>
   )
 }
 
