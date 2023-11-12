2848d731b60f8b9adeeb5e5b5068c70a7cb175b7
Sinchana P <sinchana.p.swarna@gmail.com>
2023-11-12 13:50:00 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from 'antd'
+
+function App() {
+
+  return (
+    <div>
+      <Button type="primary">Test</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { ConfigProvider } from "antd"
+import "antd/dist/reset.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ConfigProvider 
+      theme={{
+        token: {
+          colorPrimary: "#2123bf",
+        },
+      }}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>,
+)
