9dd2238e58f9b7107c878144e0304ebbd688b276
Sinchana P <sinchana.p.swarna@gmail.com>
2023-11-13 11:24:26 +0530

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, Layout, Menu } from 'antd'
+import { Button, Card, Layout, Menu, Space, Typography } from 'antd'
 import { Content, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import "./styles/dashboard.css"
@@ -8,6 +8,7 @@ import { GrOrganization } from 'react-icons/gr'
 import { BsPerson } from 'react-icons/bs'
 import { FaUsers } from 'react-icons/fa'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineMoneyCollect } from 'react-icons/ai'
 
 function App() {
 
@@ -55,7 +56,38 @@ function App() {
             ]}
           />
         </Sider>
-        <Content>Content</Content>
+        <Content className='content'>
+          <Space direction="horizontal">
+            {/* <Card>
+              <Typography.Title>Sales</Typography.Title>
+              <Typography.Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet risus feugiat in ante metus dictum at. Dui vivamus arcu felis bibendum ut tristique. Eu feugiat pretium nibh ipsum consequat nisl. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Convallis tellus id interdum velit laoreet id donec ultrices.
+              </Typography.Paragraph>
+            </Card> */}
+            <Card>
+              <Space direction='horizontal'>
+                <AiOutlineMoneyCollect />
+                <small>Total Sales</small>
+              </Space>
+              <Typography.Title>$234344</Typography.Title>
+            </Card>
+            <Card>
+              <Space direction='horizontal'>
+                <AiOutlineMoneyCollect />
+                <small>Total Sales</small>
+              </Space>
+              <Typography.Title>$234344</Typography.Title>
+            </Card>
+            <Card>
+              <Space direction='horizontal'>
+                <AiOutlineMoneyCollect />
+                <small>Total Sales</small>
+              </Space>
+              <Typography.Title>$234344</Typography.Title>
+            </Card>
+            
+          </Space>
+        </Content>
       </Layout>
     </Layout>
   )
